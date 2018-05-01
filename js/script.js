7acfbadc1c45d0735779adba1ca90ea604a2e004
Sid <weird_alien@protonmail.com>
2018-05-01 19:18:59 +0700

    rough request, need formatting

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 $(document).ready(function () {
-        var x = document.getElementById("location-msg");
-        var baseurl = "https://fcc-weather-api.glitch.me/";
+    var x = document.getElementById("location-msg");
+    var baseUrl = "https://fcc-weather-api.glitch.me";
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(getWeather);
+    } else {
+        x.innerHTML = "Not supported";
+    };
 
-        function getLocation() {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(showPosition);
-            } else {
-                x.innerHTML = "Geolocation is not supported by this browser.";
-            }
-        }
-
-        function showPosition(position) {
-            x.innerHTML = "Latitude: " + position.coords.latitude +
-                "&nbsp;Longitude: " + position.coords.longitude;
-        }
-
-        getLocation();
-    });
+    function getWeather(position) {
+        var lat = position.coords.latitude;
+        var lgt = position.coords.longitude;
+        var requestUrl = baseUrl + "/api/current?lon=" + lgt + "&lat=" + lat;
+        x.innerHTML = "Lat: " + lat + " | " + "Longt: " + lgt;
+        $.getJSON(requestUrl, function(json) {
+            $("#weather-msg").html(JSON.stringify(json));
+          });
+    };
+});

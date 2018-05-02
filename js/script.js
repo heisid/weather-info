678bb2ee9c182c0c5190f70d5867732bf9422286
Sid <weird_alien@protonmail.com>
2018-05-02 16:06:07 +0700

    just a lil' more

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
         var requestUrl = baseUrl + "/api/current?lon=" + position.coords.longitude + "&lat=" + position.coords.latitude;
         $.getJSON(requestUrl, function(json) {
             $("#location-msg").html("Lat: " + json.coord.lat + " | " + "Longt: " + json.coord.lon);
-            $("#main-weather-msg").html("<img src=" + json.weather[0].icon + "> " + json.weather[0].main);
+            $("#main-weather-msg").html(json.weather[0].main + "<br><img src=" + json.weather[0].icon + "> ");
             $("#weather-msg").html(JSON.stringify(json));
           });
     };

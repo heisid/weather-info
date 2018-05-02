c84ab8850d16ee0cf11f5e95225f5d6c747563d8
Sid <weird_alien@protonmail.com>
2018-05-02 22:13:16 +0700

    collapsed table

--- js/script.js
+++ js/script.js
@@ -9,9 +9,14 @@ $(document).ready(function () {
     function getWeather(position) {
         var requestUrl = baseUrl + "/api/current?lon=" + position.coords.longitude + "&lat=" + position.coords.latitude;
         $.getJSON(requestUrl, function(json) {
-            $("#location-msg").html("Lat: " + json.coord.lat + " | " + "Longt: " + json.coord.lon);
-            $("#main-weather-msg").html(json.weather[0].main + "<br><img src=" + json.weather[0].icon + "> ");
-            $("#weather-msg").html(JSON.stringify(json));
+            $("#location-msg").html("Lat: " + json.coord.lat + " | " + "Longt: " + json.coord.lon + " | " + json.name);
+            var mainWeather = json.weather[0].main + ", " + json.main.temp + "&deg;C<br><img src=" + json.weather[0].icon + "> ";
+            $("#main-weather-msg").html(mainWeather);
+            $("#td-temp").html(json.main.temp + " &deg;C");
+            $("#td-pressure").html(json.main.pressure + " mbar");
+            $("#td-humidity").html(json.main.humidity + " &percnt;");
+            $("#td-speed").html(json.wind.speed + " kmph");
+            $("#td-direction").html(json.wind.deg + " &deg;");
           });
     };
 });

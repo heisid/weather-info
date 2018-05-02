e6e366a426a3c10a11b887bb3f988c853a301eec
Sid <weird_alien@protonmail.com>
2018-05-02 07:00:37 +0700

    better

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
 $(document).ready(function () {
-    var x = document.getElementById("location-msg");
     var baseUrl = "https://fcc-weather-api.glitch.me";
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(getWeather);
     } else {
-        x.innerHTML = "Not supported";
+        $("#location-msg").html("Not supported");
     };
 
     function getWeather(position) {
-        var lat = position.coords.latitude;
-        var lgt = position.coords.longitude;
-        var requestUrl = baseUrl + "/api/current?lon=" + lgt + "&lat=" + lat;
-        x.innerHTML = "Lat: " + lat + " | " + "Longt: " + lgt;
+        var requestUrl = baseUrl + "/api/current?lon=" + position.coords.longitude + "&lat=" + position.coords.latitude;
         $.getJSON(requestUrl, function(json) {
+            $("#location-msg").html("Lat: " + json.coord.lat + " | " + "Longt: " + json.coord.lon);
+            $("#main-weather-msg").html("<img src=" + json.weather[0].icon + "> " + json.weather[0].main);
             $("#weather-msg").html(JSON.stringify(json));
           });
     };
